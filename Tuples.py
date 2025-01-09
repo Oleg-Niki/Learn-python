96672d349895bcb94820c68916307f54f2b61a7b
Oleg-Niki <nikitashin.ov@gmail.com>
2025-01-09 11:34:01 -0800

    Tuples done

--- Tuples.py
+++ Tuples.py
@@ -1,3 +1,6 @@
+from filecmp import cmp
+
+
 tuple_1 = ('a', 'b', 'c', 'd', 'e')
 print(type(tuple_1))
 print("Contents of tuple_1: ", tuple_1)
@@ -20,3 +23,16 @@ print("Elements from index 2 to end: ", tuple_1[2:])
 print("Elements from start to index 2: ", tuple_1[:3])
 print("All elements: ", tuple_1[:])
 print("Starting from the first to last with step 2: ", tuple_1[::2])
+
+# Updating a tuple
+tuple_4 = (100, 200, 300)
+print("Elements inside tuple_1 are", tuple_1)
+print("Elements inside tuple_4 are", tuple_4)
+tuple_5 = tuple_1 + tuple_4
+print("Elements inside tuple_5 are", tuple_5)
+tuple_6 = tuple_4 * 3
+print("Elements inside tuple_6 are", tuple_6)
+
+# Deleting a tuple
+del tuple_6
+print("tuple_6 is deleted")
