3ce3167c780a264c4aab9dd1ea6bc02122323a3c
Oleg-Niki <nikitashin.ov@gmail.com>
2025-03-05 11:27:58 -0800

    upd

---
+++ CIS117-Python/Lab2.py
@@ -0,0 +1,57 @@
+'''
+Objectives: 
+Work collaboratively to create a module of related functions in Python. 
+
+Look for your partners in Groups on Canvas
+Your assignment:
+This assignment is divided in two parts:
+
+Part 1: (with a partner): share the workload with your partner to create the Antics.py module
+Part 2 (individual): import the module you developed and test all function in a Jupyter Notebook
+The Antics.py module
+Create a module of functions to test alphabetical antics (or word play):
+
+Palindrome: a word or sentence that reads the same backwards
+Example: Anna, Bob, kayak, deed, rotator
+Pangram: a phrase or sentence containing all 26 letters of the alphabet
+Example: The quick brown fox jumps over the lazy dog.
+Tautogram: a text in which all words start with the same letter
+Example: Brilliant, because bacon bites beat bruschetta.
+Isogram: a word in which no letter of the alphabet occurs more than once
+Example: demographics
+Abedecerian: a word in which the letters appear in alphabetical order
+Example: bijoux, biopsy, dimpsy
+Dobloon: a word in which every letter that appears in the word appears exactly twice
+Example: murmur, noon, reappear, sees
+Each function should receive a word as an argument and return true of false. Name the module antics.py
+
+The Notebook
+Make a copy of Lab 2 notebookLinks to an external site. on your Google Drive Colab Notebooks folder.
+This part is individual
+Also upload the Antics.py module to your Google Drive Colab Notebooks folder.
+Follow the instructions to test your functions in Antics.py. 
+ 
+
+Documentation 
+In the Antics.py:
+
+Write a header comment at the beginning of the antics.py module with the following information:
+# CIS-117 Lab2
+# Write a description of your module here
+# Group#
+# Names of all group members (first and last)
+Document all functions in the module following docstring format
+In the notebook:
+
+Follow the instructions in the .ipynb for the header document there
+ 
+
+Testing:
+Follow the instructions in the notebook to import and test the module.
+
+Submission:
+The submission is individual. Turn in both:
+
+Antics.py that you completed with your partner
+Your notebook (Lab2_YourLastName_FirstName.ipynb) file
+'''
