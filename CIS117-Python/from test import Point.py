f01e3fa87b534d6b3f9228fc6c38abbf2add63ef
Oleg-Niki <nikitashin.ov@gmail.com>
2025-03-12 11:24:00 -0700

    upd after class 3/12 (on Classes in python)

---
+++ CIS117-Python/from test import Point.py
@@ -0,0 +1,13 @@
+from test import Point
+
+# Example usage of the Point class
+p1 = Point(2, 3)
+p2 = Point(5, 7)
+
+print(f"Point 1: ({p1.x}, {p1.y})")
+print(f"Point 2: ({p2.x}, {p2.y})")
+
+p3 = Point(1, 3)
+p4 = Point(3, 1)
+p5 = p3 + p4
+print(p5)
--- CIS117-Python/test.py
+++ CIS117-Python/test.py
@@ -1,30 +1,35 @@
-my_list = [1,2,3,4,1,2,3,4,5,6,8,9,5,4,3]
-print(my_list)
-my_char = 'Oleg'
-#print(set(my_list))
+class Point:
+    'class that represents a point on the coordinate plane'
+    def __init__(self, xcoord=0, ycoord=0):
+        'initialize coordinates to (xcoord, ycoord)'
+        self.x = xcoord
+        self.y = ycoord
+        
+    def setx(self, xcoord):
+        'set x coordinate to point'
+        self.x = xcoord
+        
+    def sety(self, ycoord):
+        self.y = ycoord
+        
+    def get(self):
+        'return tuple with coordinates'
+        return(self.x, self.y)
+    
+    def move(self,dx,dy):
+        self.x += dx
+        self.y += dy
+        
+    def __add__(self, p):
+        return Point(self.x + p.x, self.y + p.y)
+    
+    def __str__(self):
+        'return a string representation of the point'
+        return f"({self.x}, {self.y})"
+        
+x = Point()
+a = Point(5,6)
+b = Point(1,2)
 
-my_list.sort()
-print(my_list)
-
-my_list += [my_char]
-print(my_list)
-
-days = {"Sunday": 1, "Monday" : 2, "Tuesday" : 4}
-print(days)
-print(days["Sunday"])
-total_days = sum(days.values())
-print(total_days)
-
-#recipes = {"Indian" : {"paneer tikka masala" : {serv1 : (banana, apple, paneer)} }}
-
-def frequency(itemList):
-    counters = {}
-    for item in itemList:
-        if item in counters:
-            counters[item] += 1
-        else:
-            counters[item] = 1
-        return counters
-
-   
-print(frequency(days))
+c = a + b
+print(c)
